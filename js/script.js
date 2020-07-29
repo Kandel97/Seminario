c24bdaf9920ddb482b6fee0a4f6e55eb70bea19d
Kandel97 <kandelyandar@gmail.com>
2020-07-29 11:57:14 -0500

    version 1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function Todo(){
+  alert('¿Desea ver Todos los servicios?')
+};
+
+function manicure(){
+  alert('¿desea ver los servicios de manicure y pedicure?')
+};
+
+function depilacion(){
+  alert('¿Desea ver los servicios de depilación en cera?')
+};
+
+function estetica(){
+  alert('¿Desea ver los servicios de estética en general?')
+};
+
+
+
+
+
